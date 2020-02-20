c435f57bf29296d1f8f9d73f672a4d3e65461c8e
t6vmp7 <31556759+t6vmp7@users.noreply.github.com>
2020-02-20 18:33:39 +0800

    Add files via upload
    OS daily download checker

---
+++ OSfilechecker.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 28 02:54:57 2019
+
+@author: Ossang
+"""
+import os
+import time
+from datetime import datetime
+
+try:    #local
+    #set up download script folder path
+    script_dir = 'C:\\Users\\User\\Desktop\\0047Automate_Script\\DLpack_Overstock\\'
+    
+    #Folder of OS daily download script
+    OS_ProductInfo_dir = 'C:\\Users\\User\\Desktop\\0047Automate_Script\\DLpack_Overstock\\'
+    OS_ProductQnA_dir = 'C:\\Users\\User\\Desktop\\0047Automate_Script\\DLpack_Overstock\\'
+    OS_Replenishment_dir  = 'C:\\Users\\User\\Desktop\\0047Automate_Script\\DLpack_Overstock\\'
+    os.chdir(script_dir)
+    
+except: #0047
+    #set up download script folder path
+    script_dir = 'C:\\Users\\raymond.hung\\Documents\\Automate_Script\\DLpack_Overstock\\'
+    
+    #Folder of OS daily download script
+    OS_ProductInfo_dir = 'N:\\E Commerce\\Public Share\\Dot Com - Overstock\\Product Information\\'
+    OS_ProductQnA_dir = 'N:\\E Commerce\\Public Share\\Dot Com - Overstock\\Q&A\\'
+    OS_Replenishment_dir  = 'N:\\E Commerce\\Public Share\\Dot Com - Overstock\\Replenishment Report\\'
+    os.chdir(script_dir)
+    
+# Today's date_label for OS daily download script
+date_label = time.strftime('%Y%m%d')
+
+# Check today's download file for OS daily download script
+file_list=list(set(os.listdir(OS_ProductInfo_dir)+os.listdir(OS_ProductQnA_dir)+os.listdir(OS_Replenishment_dir)))
+
+for i in range(3):
+    count = 0
+
+    for file in file_list:
+        if 'Product Infomation '+date_label in file:
+            count+=1
+        elif 'Product Page Q & A '+date_label in file:
+            count+=1       
+        elif 'Replenishment detail '+date_label in file:
+            count+=1
+
+    if count < 3:
+        os.system("python "+script_dir+"DL_OS_ProductInfo.py")
+        os.system("python "+script_dir+"DL_OS_ProductQA.py")
+        os.system("python "+script_dir+"DL_OS_Replenishment_detail.py")
