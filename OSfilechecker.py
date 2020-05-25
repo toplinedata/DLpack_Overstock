7a06d035139aba73dcc05cd665ded98d897429b1
t6vmp7 <31556759+t6vmp7@users.noreply.github.com>
2020-05-25 09:37:31 +0800

    optimize process

--- OSfilechecker.py
+++ OSfilechecker.py
@@ -8,7 +8,8 @@ import os
 import time
 from datetime import datetime
 
-try:    #local
+try:    
+    #local
     #set up download script folder path
     script_dir = 'C:\\Users\\User\\Desktop\\0047Automate_Script\\DLpack_Overstock\\'
     
@@ -17,8 +18,8 @@ try:    #local
     OS_ProductQnA_dir = 'C:\\Users\\User\\Desktop\\0047Automate_Script\\DLpack_Overstock\\'
     OS_Replenishment_dir  = 'C:\\Users\\User\\Desktop\\0047Automate_Script\\DLpack_Overstock\\'
     os.chdir(script_dir)
-    
-except: #0047
+except: 
+    #0047
     #set up download script folder path
     script_dir = 'C:\\Users\\raymond.hung\\Documents\\Automate_Script\\DLpack_Overstock\\'
     
@@ -32,20 +33,26 @@ except: #0047
 date_label = time.strftime('%Y%m%d')
 
 # Check today's download file for OS daily download script
-file_list=list(set(os.listdir(OS_ProductInfo_dir)+os.listdir(OS_ProductQnA_dir)+os.listdir(OS_Replenishment_dir)))
-
 for i in range(3):
     count = 0
+    file_list=list(set(os.listdir(OS_ProductInfo_dir)+os.listdir(OS_ProductQnA_dir) \
+                    +os.listdir(OS_Replenishment_dir)))
 
     for file in file_list:
         if 'Product Infomation '+date_label in file:
+            print("Product Infomation OK")
             count+=1
         elif 'Product Page Q & A '+date_label in file:
+            print("Product Page Q&A OK")
             count+=1       
         elif 'Replenishment detail '+date_label in file:
+            print("Replenishment detail OK")
             count+=1
 
     if count < 3:
         os.system("python "+script_dir+"DL_OS_ProductInfo.py")
         os.system("python "+script_dir+"DL_OS_ProductQA.py")
         os.system("python "+script_dir+"DL_OS_Replenishment_detail.py")
+    else:
+        break
+
